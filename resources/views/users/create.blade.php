f856e3a273abe2770ddf486af8887ce656abd4b2
NarineHay <naromisho87@gmail.com>
2023-09-18 15:52:23 +0400

    NH_18.09.23

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 
 @section('content')
@@ -65,4 +65,115 @@
 </div>
 {!! Form::close() !!}
 
+@endsection --}}
+
+
+@extends('layouts.auth_app')
+@section('content')
+
+        <div class="pagetitle-wrapper">
+            <div class="pagetitle">
+                <h1>Ստեղծել նոր Օգտատեր</h1>
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!-- End Page Title -->
+
+        <section class="section">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center my-3"></div>
+
+                        <form class="row g-3 needs-validation myclass" novalidate>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" required placeholder="" />
+                                    <label class="form-label">Օգտագործողի անունը</label>
+                                    <div class="invalid-feedback">
+                                        Խնդրում եմ, մուտքագրեք օգտագործողի անունը:
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" required placeholder="" />
+                                    <label class="form-label">Քո էլէկտրոնային փոստը</label>
+                                    <div class="invalid-feedback">
+                                        Խնդրում ենք մուտքագրել գործող էլեկտրոնային հասցե!
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" required placeholder="" />
+                                    <label class="form-label">Անուն</label>
+
+                                    <div class="invalid-feedback">
+                                        Խնդրում ենք ընտրել ձեր անուն:
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" placeholder="" />
+                                    <label class="form-label">Ազգանուն</label>
+                                    <div class="invalid-feedback">
+                                        Խնդրում եմ, մուտքագրեք ձեր Ազգանունը:
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" required placeholder="" />
+                                    <label class="form-label">Գաղտնաբառ</label>
+
+                                    <div class="invalid-feedback">
+                                        Խնդրում ենք մուտքագրել ձեր գաղտնաբառը:
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" required placeholder="" />
+                                    <label class="form-label">Կրկնել գաղտնաբառ</label>
+
+                                    <div class="invalid-feedback">
+                                        Խնդրում ենք մուտքագրել ձեր գաղտնաբառը:
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <select name="" class="form-select">
+                                        <option selected disabled value="" hidden></option>
+                                        <option value="1">Մոդերատր</option>
+                                        <option value="1">Ադմինիստրատր</option>
+                                    </select>
+                                    <label class="form-label">Դերեր</label>
+                                    <div class="invalid-feedback">Խնդրում ենք ընտրել նշվածներից մեկը</div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 my-btn-class">
+                                <button class="btn btn-primary" type="submit">
+                                    Գրանցվել
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
 @endsection
