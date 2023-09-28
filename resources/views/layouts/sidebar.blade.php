17565b261e18ea78ac43e366bc43a7d6515515ca
AshotWebDev <poghosyan.01@list.ru>
2023-09-28 16:51:14 +0400

    dictionry

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,30 +8,7 @@
         </li>
 
         
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-menu-button-wide" title="Կարգավորումներ"></i><span>Կարգավորումներ</span><i
-                    class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="components-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
-                <li>
-                    <a href="components-alerts.html">
-                        <i class="bi bi-person" title="Օգտատերեր"></i><span>Օգտատերեր</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('roles.index')}}" >
-                        <i class="bi bi-person-gear" title="{{ __('sidebar.roles')}}"></i><span>{{ __('sidebar.roles')}}</span>
-                    </a>
-                </li>
-                <!-- <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('roles.*') ? '' : ' collapsed' }}" >
-                        <i  class="bi bi-person-gear" title="{{ __('sidebar.roles')}}"></i><span>{{ __('sidebar.roles')}}</span>
-                    </a>
-
-                </li> -->
-            </ul>
-        </li>
+        
         <li class="nav-item">
             <a class="nav-link collapsed" data-bs-target="#components-nav1" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-menu-button-wide" title="Տվյալներրի մուտք"></i><span>Տվյալներրի մուտք</span><i
@@ -56,9 +33,9 @@
 
         <!-- End file upload Page Nav -->
 
-        <!-- Bibliography -->
+        
 
-
+        <!-- dictionry start -->
         <li class="nav-item">
             <a class="nav-link collapsed" data-bs-target="#components-nav2" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-journals" title="Բառարաններ"></i><span>Բառարաններ</span><i
@@ -181,12 +158,132 @@
 
                 <li>
                     <a href="components-alerts.html">
-                        <i i class="bi bi-journal-text" title="Սեփականության բնույթը"></i><span>Ավտոմեքենայի կատեգորիան</span>
+                        <i i class="bi bi-journal-text" title="Ավտոմեքենայի մոդելը"></i><span>Ավտոմեքենայի մոդելը</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Մուտքի նպատակը, դրդապատճառը, պատճառը"></i><span>Մուտքի նպատակը, դրդապատճառը, պատճառը</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Գործողության նպատակը"></i><span>Գործողության նպատակը</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Փաստի որակավորում(գործողությունների աղյուսակ)"></i><span>Փաստի որակավորում(գործողությունների աղյուսակ)</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Գործողության տևեղությունը"></i><span>Գործողության տևեղությունը</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Գործողության կատարման պայմանները"></i><span>Գործողության կատարման պայմանները</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Հետևանք"></i><span>Հետևանք</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Իրադարձության որակավորումպհպ"></i><span>Իրադարձության որակավորում</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Օ/ա պաշտոնը"></i><span>Օ/ա պաշտոնը</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Կազմակերպության կատեգորիա"></i><span>Կազմակերպության կատեգորիա</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Ահազանգի երանգավորում"></i><span>Ահազանգի երանգավորում</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Ներգրաված ուժերը և միջոցները"></i><span>Ներգրաված ուժերը և միջոցները</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Ստուգման արթյունքները (Ահազանգ)"></i><span>Ստուգման արթյունքները (Ահազանգ)</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Կատարման արդյունքները(վերահսկում)"></i><span>Կատարման արդյունքները(վերահսկում)</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i i class="bi bi-journal-text" title="Ձեռնարկված միջոցները"></i><span>Ձեռնարկված միջոցները</span>
                     </a>
                 </li>
             </ul>
         </li>
 
+        <!-- dictionry end -->
 
+        <!-- admin -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-menu-button-wide" title="Ադմինիստրատոր"></i><span>Ադմինիստրատոր</span><i
+                    class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="components-alerts.html">
+                        <i class="bi bi-person" title="Գործածողների Ցուցակ"></i><span>Գործածողների Ցուցակ</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('roles.index')}}" >
+                        <i class="bi bi-person-gear" title="{{ __('sidebar.roles')}}"></i><span>{{ __('sidebar.roles')}}</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="components-alerts.html">
+                        <i class="bi bi-card-text" title="Գրառում"></i><span>Գրառում</span>
+                    </a>
+                </li>
+
+
+
+
+                <!-- <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('roles.*') ? '' : ' collapsed' }}" >
+                        <i  class="bi bi-person-gear" title="{{ __('sidebar.roles')}}"></i><span>{{ __('sidebar.roles')}}</span>
+                    </a>
+
+                </li> -->
+            </ul>
+        </li>
     </ul>
 </aside>
