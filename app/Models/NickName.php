75b0c3de4d6f1250319027aeb0eacd1b8b39e065
miqay <miqayelpetrosyan@gmail.com>
2023-10-16 15:59:42 +0400

    man crate fields

--- app/Models/NickName.php
+++ app/Models/NickName.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Man\Man;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class NickName extends Model
 {
@@ -14,4 +16,9 @@ class NickName extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function man(): BelongsToMany
+    {
+        return $this->belongsToMany(Man::class, 'man_has_nickname');
+    }
 }
