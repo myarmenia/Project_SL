2ef14ceb62582e7eaa2468ef842b00931a23d2e3
miqay <miqayelpetrosyan@gmail.com>
2023-10-17 16:08:42 +0400

    modal error

--- public/assets/js/man/script.js
+++ public/assets/js/man/script.js
@@ -120,40 +120,40 @@ function postFile(requestData) {
         });
         closeFuncton();
       }
-const formControl = document.querySelectorAll('.form-control')
+// const formControl = document.querySelectorAll('.form-control')
 
 const tegs = document.querySelectorAll('.Myteg span:nth-of-type(1)')
-
-formControl.forEach(input => {
-    input.addEventListener('blur', onBlur)
-})
-
-function onBlur() {
-    let newInfo = {}
-    if (this.classList.contains('intermediate')) {
-
-    } else {
-        if (this.closest('.form-floating').querySelector('.my-plus-class')) {
-            fetchInputTitle(this)
-        }
-
-        if (this.value) {
-            let newInfo = {};
-            if (this.hasAttribute('data-modelid')) {
-                const get_model_id = this.getAttribute('data-modelid')
-
-                newInfo.intermediate = 1
-            } else {
-                newInfo = {
-                    ...newInfo,
-                    value: this.value,
-                    fieldName: this.name
-                }
-            }
-        }
-    }
-    fetQuery(this.value, newInfo)
-}
+//
+// formControl.forEach(input => {
+//     input.addEventListener('blur', onBlur)
+// })
+//
+// function onBlur() {
+//     let newInfo = {}
+//     if (this.classList.contains('intermediate')) {
+//
+//     } else {
+//         if (this.closest('.form-floating').querySelector('.my-plus-class')) {
+//             fetchInputTitle(this)
+//         }
+//
+//         if (this.value) {
+//             let newInfo = {};
+//             if (this.hasAttribute('data-modelid')) {
+//                 const get_model_id = this.getAttribute('data-modelid')
+//
+//                 newInfo.intermediate = 1
+//             } else {
+//                 newInfo = {
+//                     ...newInfo,
+//                     value: this.value,
+//                     fieldName: this.name
+//                 }
+//             }
+//         }
+//     }
+//     fetQuery(this.value, newInfo)
+// }
 
 function fetQuery(value, newInfo) {
     console.info(newInfo)
