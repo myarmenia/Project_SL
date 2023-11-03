6a8196abb725aab78aeade7861653116b46316f2
Aghvan <axvanpanosyan19@gimail.com>
2023-11-03 17:15:21 +0400

    03.11

--- routes/web.php
+++ routes/web.php
@@ -347,6 +347,12 @@ Route::group(
                 return view('alarm.index');
             })->name('alarm-handling');
 
+            // =======================================
+
+            Route::get('/fusion', function () {
+              return view('fusion.index');
+          })->name('fusion');
+
               Route::get('/bibliography/summary-automatic', [SummeryAutomaticController::class, 'index'])->name('bibliography.summery_automatic');
 
             });
