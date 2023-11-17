52108b3d41fba6c881bb6ebf2764ee7630a42c21
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2023-11-17 15:05:34 +0400

    check_signal17.11.23

--- app/Services/ComponentService.php
+++ app/Services/ComponentService.php
@@ -51,6 +51,7 @@ class ComponentService
 
     public function deleteFromTable(Request $request): JsonResponse|array
     {
+
         $segments = explode('/', parse_url(url()->previous())['path']);
         $id = $request['id'];
         $pivot_table_name = $request['pivot_table_name'];
@@ -65,6 +66,7 @@ class ComponentService
         if (isset($request['relation']) && $request['relation'] === 'has_many'){
             $find_model->$pivot_table_name->find($id)->delete();
         }else{
+
             $find_model->$pivot_table_name()->detach($id);
         }
 
