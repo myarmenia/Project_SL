3ee30c752f1f6f21d496a718b2820bd8a1c09455
Aghvan <axvanpanosyan19@gimail.com>
2023-11-17 10:56:30 +0400

    change

--- resources/views/translate/index.blade.php
+++ resources/views/translate/index.blade.php
@@ -39,13 +39,13 @@
                     <div class="add_type_block">
 
                         <select class="form-select  translate-select">
-                            <option value =''>Բոլոր տիպերը</option>
+                            <option value =''>{{ __('content.all_type') }}</option>
                             @foreach ($chapters as $chapter)
                                 <option value="{{ $chapter->id }}">{{ $chapter->content }}</option>
                             @endforeach
                         </select>
 
-                        <button class="btn btn-primary"><a href="{{ route('translate.create') }}">Ավելացնել</a></button>
+                        <button class="btn btn-primary"><a href="{{ route('translate.create') }}">{{ __('content.addTo') }}</a></button>
 
                     </div>
 
@@ -63,19 +63,19 @@
                                     </th>
 
                                     <th data-sort="null" data-type="standart-complex">
-                                        Հայերեն {{-- <i class="fa fa-filter" data-field-name="armenian" data-table-name='xxx'
+                                        {{ __('content.lang_am') }} {{-- <i class="fa fa-filter" data-field-name="armenian" data-table-name='xxx'
                                             data-section-name="translate" aria-hidden="true"></i>  --}}
                                     </th>
                                     <th data-sort="null" data-type="standart-complex">
-                                        Ռուսերեն {{--  <i class="fa fa-filter" data-field-name="russian" data-table-name='xxx'
+                                        {{ __('content.lang_ru') }}{{--  <i class="fa fa-filter" data-field-name="russian" data-table-name='xxx'
                                             data-section-name="translate" aria-hidden="true"></i> --}}
                                     </th>
                                     <th data-sort="null" data-type="standart-complex">
-                                        Անգլերեն {{-- <i class="fa fa-filter" data-field-name="english" data-table-name='xxx'
+                                        {{ __('content.lang_eng') }}{{-- <i class="fa fa-filter" data-field-name="english" data-table-name='xxx'
                                             data-section-name="translate" aria-hidden="true"></i> --}}
                                     </th>
                                     <th>
-                                        Տիպ
+                                        {{ __('content.type') }}
                                     </th>
                                     <th></th>
                                 </tr>
@@ -83,12 +83,11 @@
                             <tbody class="table_tbody">
                                 @foreach ($data as $item)
                                     <tr>
-
-                                        <td class="trId">{{ $item->id }}</td>
-                                        <td class="tdTxt">{{ $item->armenian }}</td>
-                                        <td class="tdTxt">{{ $item->russian }}</td>
-                                        <td class="tdTxt">{{ $item->english }}</td>
-                                        <td class="tdTxt">{{ $item->chapter->content }}</td>
+                                        <td class="trId">{{$item->id}}</td>
+                                        <td class="tdTxt">{{$item->armenian}}</td>
+                                        <td class="tdTxt">{{$item->russian}}</td>
+                                        <td class="tdTxt">{{$item->english}}</td>
+                                        <td class="tdTxt">{{$item->chapter->content}}</td>
                                         <td><i class="bi bi-pencil-square etid-icon" title="խմբագրել" data-bs-toggle="modal"
                                                 data-bs-target="#exampleModazl" data-bs-whatever="@mdo"></i></td>
                                     </tr>
@@ -103,123 +102,6 @@
             </div>
         </div>
     </section>
-
-    <!-- modal block -->
-    <div class="modal" id="deleteModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="close close_modal" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" id="close_button"
-                        data-bs-dismiss="modal">Չեղարկել</button>
-                    <form action="" id="delete_form">
-                        <button class="btn btn-primary" id="delete_button" data-bs-dismiss="modal">Հաստատել</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- modal range -->
-
-
-    <div class="modal" id="avtiveModal" tabindex="-1" role="dialog">
-
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="close close_modal" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" id="cancel_btn"
-                        data-bs-dismiss="modal">Չեղարկել</button>
-                    <form action="" id="status_form">
-                        <button type="button" class="btn btn-primary" id="isActive_button"
-                            data-bs-dismiss="modal">Հաստատել</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="modal fade" id="exampleModalLg" tabindex="-1" aria-labelledby="exampleModalLgLabel"
-        style="display: none;" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title h4" id="exampleModalLgLabel">Ավելացնել նոր գրառում</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form class='my-form-class' action="" method="Post">
-                        @csrf
-                        <div>
-                            <div class="form-floating">
-                                <input type="text" name="name"
-                                    class="form-control @error('name') error-border @enderror" placeholder="" />
-                                <label class="form-label">Անվանում</label>
-                            </div>
-                            @error('name')
-                                <div class="error-text">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
-
-                        <button class='btn btn-primary my-class-sub'>Ավելացնել</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-    </div>
-    {{-- edit modal blog --}}
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Change</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form class="translate-form">
-                        <div class="mb-3">
-                            <label for="recipient-name" class="col-form-label">Հայերեն:</label>
-                            <input type="text" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="recipient-name" class="col-form-label">Ռուսերեն:</label>
-                            <input type="text" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="recipient-name" class="col-form-label">Անգլերեն:</label>
-                            <input type="text" class="form-control">
-                        </div>
-
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary close-btn" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary edit-btn" data-bs-dismiss="modal">Edit</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
 @section('js-scripts')
     <script>
