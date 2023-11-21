ab16a2ff7e196a51a6c5d1fe71f59341ecc1349b
Aghvan <axvanpanosyan19@gimail.com>
2023-11-21 16:26:49 +0400

    aghvan

--- public/assets/js/search-file/search-file.js
+++ public/assets/js/search-file/search-file.js
@@ -1,11 +1,13 @@
-// const serachButton = document.getElementById("serach_button");
-// let cardBody = document.querySelector(".card-body");
+const serachButton = document.getElementById("serach_button");
+let cardBody = document.querySelector(".card-body");
 
-// serachButton.addEventListener("click", () => {
-//   let activTable = document.querySelector(".table");
-//   if (!activTable) {
+serachButton.addEventListener("click", () => {
+  let activTable = document.querySelector(".table");
+  if (!activTable) {
+    
+    let searchButtonInp = document.getElementById("search_text");
+    let searchInput = document.getElementById("search_input");
 
-<<<<<<< HEAD
     let searchTextDiv = document.createElement("div");
     searchTextDiv.textContent = searchInput.value;
     searchTextDiv.style = `
@@ -25,22 +27,11 @@
     let table = document.createElement("table");
     let tbody = document.createElement("tbody");
     let thead = document.createElement("thead");
-=======
-//     let searchButtonInp = document.getElementById("search_text");
-//     let searchInput = document.getElementById("search_input");
->>>>>>> 3abba85d42919e0d87ba54ad4406c1d0c19d8a5d
 
-//     let searchTextDiv = document.createElement("div");
-//     searchTextDiv.textContent = searchInput.value;
-//     searchTextDiv.style = `
-//       border: 1px solid;
-//       width: fit-content;
-//       padding: 6px;
-//       border-radius: 10px;
-//   `;
-//     searchButtonInp.insertAdjacentElement("afterend", searchTextDiv);
+    table.className = "person_table table";
+    table.style.marginTop = "30px";
+    let trTh = document.createElement("tr");
 
-<<<<<<< HEAD
     trTh.innerHTML = `
     <th class="input-th change-th change-all-input" style="text-align: center"><input type="checkbox" class="form-check-input"/></th>
       <th>id</th>
@@ -49,23 +40,10 @@
       <th>${fileName}</th>
       <th>${contactPerson}</th>
   `;
-=======
-//     ///////////table create ////////////////
-//     let div = document.createElement("div");
-//     div.style = `
-//   overflow: auto;
-//   padding-bottom: 20px;
-//   `;
-//     let table = document.createElement("table");
-//     let tbody = document.createElement("tbody");
-//     let thead = document.createElement("thead");
->>>>>>> 3abba85d42919e0d87ba54ad4406c1d0c19d8a5d
 
-//     table.className = "person_table table";
-//     table.style.marginTop = "30px";
-//     let trTh = document.createElement("tr");
+    thead.appendChild(trTh);
+    table.appendChild(thead);
 
-<<<<<<< HEAD
     let trTd = document.createElement("tr");
     trTd.innerHTML = `
     <td class="input-td change-td" style="text-align: center"><input type="checkbox" class="form-check-input"/></td>
@@ -135,38 +113,3 @@ similarity_input.addEventListener('change' , (e) => {
 
 
 // ============== search input js end == //
-=======
-//     trTh.innerHTML = `
-//       <th>id</th>
-//       <th>${association}</th>
-//       <th>${keyword}</th>
-//       <th>${fileName}</th>
-//       <th>${contactPerson}</th>
-//   `;
-
-//     thead.appendChild(trTh);
-//     table.appendChild(thead);
-
-//     let trTd = document.createElement("tr");
-//     trTd.innerHTML = `
-// <td>tiv</td>
-// <td class="input-td change-td" style="text-align: center"><input type="checkbox" class="form-check-input"/></td>
-// <td class="input-td change-td" >${searchInput.value}</td>
-// <td class="input-td change-td" >cccc</td>
-// <td class ='change-td-btn'></td>
-// `;
-//     tbody.appendChild(trTd);
-//     table.appendChild(tbody);
-//     div.appendChild(table);
-//     searchInput.value =""
-
-//     let button = document.createElement("button");
-//     button.className = "btn btn-primary";
-//     button.textContent = create_response;
-//     button.style.marginTop = "32px";
-//     table.appendChild(button);
-//     cardBody.appendChild(div);
-//   }
-
-// });
->>>>>>> 3abba85d42919e0d87ba54ad4406c1d0c19d8a5d
