f5a5ae1253bb94e71ae03cc413424ec80df9ed2e
NarineHay <naromisho87@gmail.com>
2023-12-01 11:19:42 +0400

    NH_01.11.23

--- resources/views/components/back-previous-url.blade.php
+++ resources/views/components/back-previous-url.blade.php
@@ -1,13 +1,21 @@
 <div class="flex justify-content-end">
-    @if($submit)
+    @if ($submit)
         <button type="submit" class="btn btn-primary"><i class="bi bi-arrow-left"></i></button>
     @else
-        <a class="btn btn-primary" href="#" onclick="history.back();return false;">
+        {{-- <a class="btn btn-primary" href="#" onclick="history.back();return false;">
+            <i class="bi bi-arrow-left"></i>
+        </a> --}}
+        <a class="btn btn-primary" href="#" id="backUrl">
             <i class="bi bi-arrow-left"></i>
         </a>
     @endif
 </div>
 
 <script>
-    sessionStorage.setItem('reload', 'yes');
+    if (document.getElementById('backUrl') != 'undefined') {
+        let breadcrumb_items = document.querySelectorAll('.breadcrumb-item')
+        let prev_url = breadcrumb_items[breadcrumb_items.length - 2].querySelector('a').getAttribute('href')
+        document.getElementById('backUrl').setAttribute('href', prev_url)
+    }
+    // sessionStorage.setItem('reload', 'yes');
 </script>
