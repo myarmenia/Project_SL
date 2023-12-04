8ec022da90d8af097259f5619b04a78a7ca7cd66
NarineHay <naromisho87@gmail.com>
2023-12-04 11:43:41 +0400

    NH_04.11.23

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -6,7 +6,6 @@ use Closure;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
 
 class SetLocale
 {
@@ -24,47 +23,6 @@ class SetLocale
             \abort(400);
         }
 
-        // $arr = [];
-
-        // if (session()->has('arr_url')) {
-        //     $arr = Session::get('arr_url');
-        // }
-        // $currentURL = url()->current();
-        // $url = explode("$language", url()->current())[1];
-        // // dd( $arr);
-        // $found = array_filter($arr, function ($v, $k) use ($url) {
-        //     return $v['url'] == $url;
-        // }, ARRAY_FILTER_USE_BOTH);
-
-
-        // if (count($found) > 0) {
-        //     $arr = array_slice($arr, 0, array_keys($found)[0]);
-        // }
-        // if (!str_contains($currentURL, 'create')) {
-        //     if (request()->segment(2) == 'open') {
-        //         $arr_asoc['url'] = $url;
-        //         $arr_asoc['name'] = request()->segment(3);
-        //         array_push($arr, $arr_asoc);
-        //     }
-
-        //     if (request()->segment(2) == 'dictionary') {
-        //         $arr_asoc['url'] = $url;
-        //         $arr_asoc['name'] = request()->segment(3);
-        //         $arr[0] = $arr_asoc;
-        //     }
-        //     if (!str_contains($currentURL, 'edit')) {
-        //         $arr_asoc['url'] = $url;
-        //         $arr_asoc['name'] = request()->segment(2);
-        //         $arr_asoc['id'] = request()->segment(3);
-        //         array_push($arr, $arr_asoc);
-        //     }
-
-
-
-
-        //     Session::put('arr_url', $arr);
-        // }
-        // dd(Session::get('arr_url'));
         App::setLocale($language);
         URL::defaults(['locale' => app()->getLocale()]);
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::group(
             })->name('searche');
         });
 
-        Route::group(['middleware' => ['auth', 'rolesNotEqualForSearch']], function () {
+        Route::group(['middleware' => ['auth', 'breadcrumbs', 'rolesNotEqualForSearch']], function () {
             Route::get('translate/index', [TranslateController::class, 'index'])->name('translate.index');
             Route::get('translate/create', [TranslateController::class, 'create'])->name('translate.create');
             Route::get('translate/edit/{id}', [TranslateController::class, 'edit'])->name('translate.edit');
