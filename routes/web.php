4a6c0e4ee85e453f003313fcb26059f7cd53044c
KirakosyanSuren <kirakosyan.suren@inbox.ru>
2023-12-10 12:53:04 +0400

    10.12

--- routes/web.php
+++ routes/web.php
@@ -417,6 +417,7 @@ Route::group(
             Route::get('open/redirect', [OpenController::class, 'redirect'])->name('open.redirect');
 
             Route::get('open/{page}', [OpenController::class, 'index'])->name('open.page');
+            Route::get('open/{page}/{id}', [OpenController::class, 'openWithBibliography'])->name('open.page.bibliography');
 
 
             // Route::get('open/{page}/{id}', [OpenController::class, 'restore'])->name('open.page.restore');
