c3a390581fe03e2790a3ed139b2e15a48a2822e4
Miqayel <miqayelpetrosyan@gmail.com>
2023-12-15 15:51:25 +0400

    fix tags/relations

--- resources/views/components/tegs-relations.blade.php
+++ resources/views/components/tegs-relations.blade.php
@@ -3,7 +3,7 @@
         @if($model->$relation instanceof \Illuminate\Database\Eloquent\Collection)
             <x-tegs :data="$model" :relation="$relation" name="id" :label="__('content.'.$relation)" :tableName="$relation" related/>
         @else
-            <x-teg :item="$model" :relation="$otherRelation ?? $relation" :relations="$relations" name="id" :labels="$labels[$key]" :tableName="$tableName" related/>
+            <x-teg :item="$model" :relation="$otherRelation ?? $relation" :relations="$relations" name="id" :labels="isset($labels) ? $labels[$key] : null" :tableName="$tableName" related/>
         @endif
     @endforeach
 </div>
