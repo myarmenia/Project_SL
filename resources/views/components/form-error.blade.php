727def650f9745994712f9cb7050ea2d354afc4f
Miqayel <miqayelpetrosyan@gmail.com>
2023-12-20 14:39:43 +0400

    fix error

--- resources/views/components/form-error.blade.php
+++ resources/views/components/form-error.blade.php
@@ -1,5 +1,7 @@
-@foreach($errors->all() as $error)
-    <div class="alert alert-danger mt-2" role="alert">
-        {{$error}}
-    </div>
-@endforeach
+@if(count($errors->all()))
+    @foreach($errors->all() as $error)
+        <div class="alert alert-danger mt-2" role="alert">
+            {{$error}}
+        </div>
+    @endforeach
+@endif
